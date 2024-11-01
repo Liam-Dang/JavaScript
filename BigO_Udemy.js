70732ff660bd64f4e230d85f30185a4a6d9e82c3
kha21 <kha2117185@maricopa.edu>
2024-11-01 11:58:28 -0700

    BigO

--- BigO_Udemy.js
+++ BigO_Udemy.js
@@ -1,17 +1,15 @@
 function findNemo(array) {
-    let t0 = performance.now();
+
     for (let i = 0; i< array.length; i++) {
         if (array[i] === 'nemo') {
             console.log('Found NEMO!');
             console.log('how many time did it loop '+ (i+1));
         }
     }
-    let t1 = performance.now();
-    console.log('Call to find Nemo took ' + (t1-t0) + ' milliseconds');
 }
 
 const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla'];
 const large = new Array(10000).fill('nemo');
 
-findNemo(large);
+findNemo(large); // O(n) --> Linear Time
